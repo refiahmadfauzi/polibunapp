550b8f051fc3fcda55fa985f42f7535657420e75
Refi Ahmad Fauzi <refiahmad05@gmail.com>
2026-02-25 20:26:55 +0700

    update logo warna

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,8 +29,36 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->brandLogo(asset('img/logo-best-rmbg.png'))
+            ->brandLogoHeight('3rem')
+            ->favicon(asset('img/logo-best-rmbg.png'))
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => [
+                    50 => '#fef2f2',
+                    100 => '#fee2e2',
+                    200 => '#fecaca',
+                    300 => '#fca5a5',
+                    400 => '#f87171',
+                    500 => '#ef4444',
+                    600 => '#dc2626',
+                    700 => '#b91c1c',
+                    800 => '#991b1b',
+                    900 => '#7f1d1d',
+                    950 => '#450a0a',
+                ],
+                'info' => [
+                    50 => '#eff6ff',
+                    100 => '#dbeafe',
+                    200 => '#bfdbfe',
+                    300 => '#93c5fd',
+                    400 => '#60a5fa',
+                    500 => '#3b82f6',
+                    600 => '#2563eb',
+                    700 => '#1d4ed8',
+                    800 => '#1e40af',
+                    900 => '#1e3a8a',
+                    950 => '#172554',
+                ],
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
